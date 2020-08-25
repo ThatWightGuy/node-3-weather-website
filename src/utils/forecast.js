958ac0762936d13b3f8d1b69d303579591359db1
Travis Wight <tman.wight@gmail.com>
2020-08-25 15:26:26 -0700

    Added data to forcast.js output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location! Please try another search!');
         } else{
-            const {weather_descriptions, temperature, feelslike} = body.current;
+            const {weather_descriptions, temperature, feelslike, observation_time} = body.current;
 
-            callback(undefined, weather_descriptions[0] + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degrees.');
+            callback(undefined, 'Current Time ' + observation_time + '. ' + weather_descriptions[0] + '. It is currently ' + temperature + ' degress out. It feels like ' + feelslike + ' degrees.');
         }
     }); 
 }
